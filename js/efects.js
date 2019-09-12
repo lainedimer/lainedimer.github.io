321a299f75180792a46a6392c7142ab3c8f52da4
lainedimer <laainediimer@gmail.com>
2019-09-12 13:17:27 -0300

    parallax effect implemented

---
+++ js/efects.js
@@ -0,0 +1,77 @@
+const menuItems = document.querySelectorAll('.menu a[href^="#"]');
+var top = document.querySelector('.scrollTop');
+
+menuItems.forEach(item => {
+		item.addEventListener('click', scrolloIdOnClick);
+	})
+	function scrolloIdOnClick(event){
+		event.preventDefault();
+		const element = event.target;
+		const id = element.getAttribute('href');
+		const to = document.querySelector(id).offsetTop;
+	
+		window.scroll({
+			top:to,
+			behavior: "smooth",
+		});
+	}
+
+	
+function parallax(){	
+	var skills = document.getElementById('skills');
+	var interests = document.getElementById('interests');
+	var skillsInte = document.getElementById('skills-interests');
+	var about = document.getElementById('about-me');
+	var experience = document.getElementById('experience');	
+	skills.style.top = -(window.pageYOffset/4) + 'px';
+	interests.style.top = (window.pageYOffset/2) + 'px';	
+	
+	if (this.scrollY>lastScrollTop){
+		console.log("valor do:" +this.lastScrollTop);
+		console.log("ScrollY:" +this.scrollY);
+		console.log("desceu");		
+		skills.style.opacity = i;
+		interests.style.opacity = i;
+		if(this.scrollY===0){
+			i=0;
+		}else if(this.scrollY>experience.offsetTop || this.scrollY===experience.offsetTop ){
+			skills.style.opacity = i;		
+			interests.style.opacity = i;
+			i= 0;
+		}else if(this.scrollY>about.offsetTop){
+			skills.style.opacity = i;
+			interests.style.opacity = i;
+  			i += 0.01;
+		}else if (this.scrollY===skillsInte.offsetTop){
+			i=1;
+			skills.style.opacity = i;		
+			interests.style.opacity = i;
+		}else{
+			//i-=0.01;
+			console.log("Nao esperado");
+		}
+		
+
+	}else{
+		i += 0.02;
+		skills.style.opacity = i;
+		interests.style.opacity = i;
+		if(this.scrollY<about.offsetTop){
+			i=0;
+			skills.style.opacity = i;
+			interests.style.opacity = i;
+		}else{
+			console.log("Nao esperado");
+		}
+
+	}
+
+	lastScrollTop = this.scrollY;
+
+}
+	
+var i=0;
+var lastScrollTop = 0;
+window.addEventListener("scroll", parallax, false);
+
+
